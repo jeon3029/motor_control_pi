1c8658a1724c3cf7b3b8ae993397938069ba964c
taejoonJeon <taejjeon@gmail.com>
2020-06-17 11:29:42 +0900

    using indent as tabs

--- pi_motor/car_db.py
+++ pi_motor/car_db.py
@@ -25,21 +25,21 @@ print("sensehat init ok")
 # TODO: 온도 정보 db에 전달
 
 class pollingThread(QThread):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-    def run(self):
-        self.db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
-        self.db.setHostName("3.34.124.67")
-        self.db.setDatabaseName("15_10")
-        self.db.setUserName("15_10")
-        self.db.setPassword("1234")
-        ok = self.db.open()
-        print(ok)
-        self.getQuery()
+	def run(self):
+		self.db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
+		self.db.setHostName("3.34.124.67")
+		self.db.setDatabaseName("15_10")
+		self.db.setUserName("15_10")
+		self.db.setPassword("1234")
+		ok = self.db.open()
+		print(ok)
+		self.getQuery()
 
-    def getQuery(self):
-      while True:
+	def getQuery(self):
+		while True:
 			time.sleep(0.1)
 			#----------------motor hat ----------------------
 			query = QtSql.QSqlQuery("select * from command1 order by time desc limit 1");
@@ -50,7 +50,7 @@ class pollingThread(QThread):
 			is_finish = query.record().value(3)
 
 			if is_finish == 0 :
-              #detect new command
+              	#detect new command
 				print(cmdTime.toString(), cmdType, cmdArg)
 
 				#update
@@ -85,61 +85,61 @@ class pollingThread(QThread):
 				for i in range(count):
 					sen.show_message(cmdText)
 
-    def go(self):
-        print("MOTOR GO")
-        dcMotor.run(Raspi_MotorHAT.FORWARD)
+	def go(self):
+		print("MOTOR GO")
+		dcMotor.run(Raspi_MotorHAT.FORWARD)
 	#time.sleep(1)
 	#dcMotor.run(Raspi_MotorHAT.RELEASE)
   
-    def stop(self):
-        print("MOTOR STOP")
-        dcMotor.run(Raspi_MotorHAT.RELEASE)
+	def stop(self):
+		print("MOTOR STOP")
+		dcMotor.run(Raspi_MotorHAT.RELEASE)
 
 
-    def down(self):
-        print("MOTOR BACK")
-        dcMotor.run(Raspi_MotorHAT.BACKWARD)
-        time.sleep(1)
-        dcMotor.run(Raspi_MotorHAT.RELEASE)
+	def down(self):
+		print("MOTOR BACK")
+		dcMotor.run(Raspi_MotorHAT.BACKWARD)
+		time.sleep(1)
+		dcMotor.run(Raspi_MotorHAT.RELEASE)
 
-    def left(self):
-        steer(-30)
-        print("MOTOR LEFT")
+	def left(self):
+		steer(-30)
+		print("MOTOR LEFT")
 
-    def right(self):
-        steer(30)
-        print("MOTOR RIGHT")
+	def right(self):
+		steer(30)
+		print("MOTOR RIGHT")
 
-    def mid(self):
-        steer(0)
-        print("MOTOR MID")
+	def mid(self):
+		steer(0)
+		print("MOTOR MID")
 
 
 
 def steer(angle=0): #
-    if angle <= -30: #
-        angle = -30
+	if angle <= -30: #
+		angle = -30
 
-    if angle >= 30:
-        angle = 30
+	if angle >= 30:
+		angle = 30
 
-    pulse_time = mid_center
+	pulse_time = mid_center
 
-    if angle == 0 :
-        pulse_time = mid_center
-        servo.setPWM(0,0,mid_center)
+	if angle == 0 :
+		pulse_time = mid_center
+		servo.setPWM(0,0,mid_center)
 
-    elif angle > 0 : # LEFT
-        #a2pul = int(angle*L_itv/30) + mid_center
-        pulse_time = int(angle*L_itv/30) + mid_center
-        servo.setPWM(0,0,pulse_time)
+	elif angle > 0 : # LEFT
+	#a2pul = int(angle*L_itv/30) + mid_center
+		pulse_time = int(angle*L_itv/30) + mid_center
+		servo.setPWM(0,0,pulse_time)
 
-    elif angle < 0 : #RIGHT
-        pulse_time = int(angle*R_itv/30)*(-1) + mid_center
-        servo.setPWM(0,0,pulse_time)
+	elif angle < 0 : #RIGHT
+		pulse_time = int(angle*R_itv/30)*(-1) + mid_center
+		servo.setPWM(0,0,pulse_time)
 
-    else :
-        servo.setPWM(0,0,pulse_time)
+	else :
+		servo.setPWM(0,0,pulse_time)
     #pulse_time = 170+(340-200)//180*(angle+90)
     #servo.setPWM(0,0,pulse_time)
 
@@ -150,4 +150,4 @@ th.start()
 
 #infinity loop
 while True:
-    pass
+	pass
